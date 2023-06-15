ebb997347c9f21dfdde15fb8e191ba1527f9a179
Paulo Roberto Farah <paulorfarah@gmail.com>
2023-06-15 19:05:11 -0300

    charts fixed first chart

--- 9.history_ml/charts.py
+++ 9.history_ml/charts.py
@@ -1,19 +1,24 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 pd.set_option('display.max_columns', None)
-metrics = ['F1-Score', 'Accuracy', 'Sensitivity', 'Specificity', 'ROC AUC score']
+metrics = ['F1-Score']#, 'Accuracy', 'Sensitivity', 'Specificity', 'ROC AUC score']
 for metric in metrics:
-    df = pd.read_csv('charts/resultadospormodelo.csv', decimal=",")
+    df = pd.read_csv('results/resultadospormodelo.csv', decimal=",")
     print(df.columns)
+    df.columns = ['Dataset', 'Algoritm', 'window', 'model', 'resample', 'F1-Score(micro)',
+       'F1-Score(macro)', 'F1-Score', 'F1-Score(None)', 'Accuracy',
+       'Sensitivity', 'Specificity', 'ROC AUC score', 'Confusion matrix']
     print('######### ' + metric)
 
     # approach
     dfApp = df.query("model == 'model1'")
-    dfApp['window'] = dfApp['window'].replace({0: 'trad', 3: 'our'})
+    # dfApp['window'] = dfApp['window'].replace({0: 'trad', 3: 'our'})
     # dfAvg = dfApp.groupby(['Dataset', 'window'])[metric].mean()
     # print(dfAvg)
     print(dfApp.head)
-    ax = pd.pivot_table(dfApp, index='Dataset', columns='window')[metric].plot.bar(alpha=0.75, rot=0)
+    ax = pd.pivot_table(dfApp, index='Dataset', columns='window', values=metric).plot.bar(alpha=0.75, rot=0)
+    # ax = pd.pivot_table(dfApp, index='Dataset', columns='window')[metric].plot.bar(alpha=0.75, rot=0)
+
     # for p in ax.patches:
     #     ax.annotate(str(p.get_height()), (p.get_x() * 1.005, p.get_height() * 1.005))
     ax.set_ylabel(metric)
@@ -36,33 +41,33 @@ for metric in metrics:
     plt.legend(title='Approach', loc='lower right')
     plt.savefig('results/window-set2'+metric+'.pdf')
 
-     # -- set 6
-    dfApp = df.query("model == 'model6'")
-    dfApp['window'] = dfApp['window'].replace({0: 'trad', 3: 'our'})
-    # dfAvg = dfApp.groupby(['Dataset', 'window'])[metric].mean()
-    # print(dfAvg)
+    # # -- set 6
+    # dfApp = df.query("model == 'model6'")
+    # dfApp['window'] = dfApp['window'].replace({0: 'trad', 3: 'our'})
+    # # dfAvg = dfApp.groupby(['Dataset', 'window'])[metric].mean()
+    # # print(dfAvg)
+    #
+    # ax = pd.pivot_table(dfApp, index='Dataset', columns='window')[metric].plot.bar(alpha=0.75, rot=0)
+    # # for p in ax.patches:
+    # #     ax.annotate(str(p.get_height()), (p.get_x() * 1.005, p.get_height() * 1.005))
+    # ax.set_ylabel(metric)
+    # ax.set_xlabel('')
+    # plt.legend(title='Approach', loc='lower right')
+    # plt.savefig('results/window-set6'+metric+'.pdf')
 
-    ax = pd.pivot_table(dfApp, index='Dataset', columns='window')[metric].plot.bar(alpha=0.75, rot=0)
-    # for p in ax.patches:
-    #     ax.annotate(str(p.get_height()), (p.get_x() * 1.005, p.get_height() * 1.005))
-    ax.set_ylabel(metric)
-    ax.set_xlabel('')
-    plt.legend(title='Approach', loc='lower right')
-    plt.savefig('results/window-set6'+metric+'.pdf')
-
-     # -- set 7
-    dfApp = df.query("model == 'model7'")
-    dfApp['window'] = dfApp['window'].replace({0: 'trad', 3: 'our'})
-    # dfAvg = dfApp.groupby(['Dataset', 'window'])[metric].mean()
-    # print(dfAvg)
-
-    ax = pd.pivot_table(dfApp, index='Dataset', columns='window')[metric].plot.bar(alpha=0.75, rot=0)
-    # for p in ax.patches:
-    #     ax.annotate(str(p.get_height()), (p.get_x() * 1.005, p.get_height() * 1.005))
-    ax.set_ylabel(metric)
-    ax.set_xlabel('')
-    plt.legend(title='Approach', loc='lower right')
-    plt.savefig('results/window-set7'+metric+'.pdf')
+    #  # -- set 7
+    # dfApp = df.query("model == 'model7'")
+    # dfApp['window'] = dfApp['window'].replace({0: 'trad', 3: 'our'})
+    # # dfAvg = dfApp.groupby(['Dataset', 'window'])[metric].mean()
+    # # print(dfAvg)
+    #
+    # ax = pd.pivot_table(dfApp, index='Dataset', columns='window')[metric].plot.bar(alpha=0.75, rot=0)
+    # # for p in ax.patches:
+    # #     ax.annotate(str(p.get_height()), (p.get_x() * 1.005, p.get_height() * 1.005))
+    # ax.set_ylabel(metric)
+    # ax.set_xlabel('')
+    # plt.legend(title='Approach', loc='lower right')
+    # plt.savefig('results/window-set7'+metric+'.pdf')
 
 
     dfApp['window'] = df['window'].replace({0: 'trad', 3: 'our'})
